1c7bb3f27b49431a426441a83d04d9c2822f9ee3
Tatiana <varlamova.com@gmail.com>
2021-04-12 10:56:01 -0500

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAPAndroid()
+	{
+		System.out.println("NativeAPAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/RESRAPITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESRAPITest {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+}
